5e481a56ea9852f3c30cba40462a8b26b6a4a5d9
AJ-West <gamingisnow73@gmail.com>
2025-10-24 14:58:03 +0100

    added basic screen render to begin with

---
+++ Depot delta/Depot delta.cpp
@@ -0,0 +1,68 @@
+// Depot delta.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3_image/SDL_image.h>
+
+using namespace std;
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+bool isRunning;
+
+//creates the window, renderer and font for the game
+void init_environment() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cerr << "SDL could not initialize: SDL_ERROR" << SDL_GetError() << endl;
+        isRunning = false;
+        return;
+    }
+    if (TTF_Init() == -1) {
+        cerr << "SDL_ttf could not initialize: TTF_ERROR" << SDL_GetError() << endl;
+        isRunning = false;
+        return;
+    }
+    window = SDL_CreateWindow("Depot delta", 1440, 900, SDL_WINDOW_RESIZABLE);
+    if (window == nullptr) {
+        cerr << "Window cannot be created: SDL_ERROR" << SDL_GetError() << endl;
+        isRunning = false;
+        return;
+    }
+    renderer = SDL_CreateRenderer(window, NULL);
+    if (renderer == nullptr) {
+        cerr << "Renderer cannot be created: SDL_ERROR" << SDL_GetError() << endl;
+        isRunning = false;
+        return;
+    }
+    isRunning = true;
+}
+
+int main()
+{
+    init_environment();
+
+    while (isRunning) {
+        //handle input
+        SDL_Event event;
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_EVENT_QUIT) {
+                isRunning = false;
+            }
+        }
+    }
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
