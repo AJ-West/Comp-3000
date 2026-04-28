9b2ee7b00cb9a1c51ebe237dec02173b318a9bd5
AJ-West <ashtonjwest2003@gmail.com>
2026-04-28 14:13:49 +0100

    added ui for level select screen

--- Depot delta/gameFiles/gameManager.h
+++ Depot delta/gameFiles/gameManager.h
@@ -6,7 +6,17 @@
 
 enum gameState {
 	Home,
+	LevelSelect,
 	Level
+};
+
+enum levels {
+	NOLVL,
+	TUTORIAL,
+	LEVEL1,
+	LEVEL2,
+	LEVEL3,
+	CONTINUE,
 };
 
 class GameManager {
@@ -24,6 +34,30 @@ public:
 		startButton = SDL_CreateTextureFromSurface(renderer, surface);
 	}
 
+	void createLevelSelectScreen() {
+		background = loadTexture("draftArt/TitleScreen/homeScreen.png");
+
+		string text = "Tutorial";
+		SDL_Surface* surface = TTF_RenderText_Solid(font, text.c_str(), text.length(), { 0,0,0,255 });
+		tutButton = SDL_CreateTextureFromSurface(renderer, surface);
+
+		text = "Level 1";
+		surface = TTF_RenderText_Solid(font, text.c_str(), text.length(), { 0,0,0,255 });
+		lvl1Button = SDL_CreateTextureFromSurface(renderer, surface);
+
+		text = "Level 2";
+		surface = TTF_RenderText_Solid(font, text.c_str(), text.length(), { 0,0,0,255 });
+		lvl2Button = SDL_CreateTextureFromSurface(renderer, surface);
+
+		text = "Level 3";
+		surface = TTF_RenderText_Solid(font, text.c_str(), text.length(), { 0,0,0,255 });
+		lvl3Button = SDL_CreateTextureFromSurface(renderer, surface);
+
+		text = "Continue";
+		surface = TTF_RenderText_Solid(font, text.c_str(), text.length(), { 0,0,0,255 });
+		conButton = SDL_CreateTextureFromSurface(renderer, surface);
+	}
+
 	void startLevel() {
 		lManager = new LevelManager(renderer);
 		currGameState = Level;
@@ -34,7 +68,16 @@ public:
 		{
 		case Home:
 			if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN && event.button.button == SDL_BUTTON_LEFT) {
-				if (checkStartClick()) {
+				if (checkClick(startButtonSize)) {
+					currGameState = LevelSelect;
+					createLevelSelectScreen();
+				}
+			}
+			break;
+		case LevelSelect:
+			if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN && event.button.button == SDL_BUTTON_LEFT) {
+				int clicked = checkLevelClick();
+				if (clicked != NOLVL) {
 					currGameState = Level;
 					lManager = new LevelManager(renderer);
 				}
@@ -46,15 +89,24 @@ public:
 		}
 	}
 
-	bool checkStartClick() {
+	bool checkClick(SDL_FRect button) {
 		//get click positon
 		float cx, cy;
 		getUIScaledMousePos(&cx, &cy);
-		if (cx < startButtonSize.x) return false;
-		if (cx > startButtonSize.x + startButtonSize.w) return false;
-		if (cy < startButtonSize.y) return false;
-		if (cy > startButtonSize.y + startButtonSize.h) return false;
+		if (cx < button.x) return false;
+		if (cx > button.x + button.w) return false;
+		if (cy < button.y) return false;
+		if (cy > button.y + button.h) return false;
 		return true;
+	}
+
+	int checkLevelClick() { // identifies if a level was selected which level
+		if (checkClick(tutButtonSize)) return TUTORIAL;
+		else if (checkClick(lvl1ButtonSize)) return LEVEL1; 
+		else if (checkClick(lvl2ButtonSize)) return LEVEL2; 
+		else if (checkClick(lvl3ButtonSize)) return LEVEL3; 
+		else if (checkClick(conButtonSize)) return CONTINUE; 
+		return NOLVL;
 	}
 
 	void render() {
@@ -62,6 +114,9 @@ public:
 		{
 		case Home:
 			renderHomeScreen();
+			break;
+		case LevelSelect:
+			renderLevelSelect();
 			break;
 		case Level:
 			lManager->render();
@@ -91,6 +146,26 @@ public:
 		SDL_RenderTexture(renderer, startButton, NULL, &startButtonSize);
 	}
 
+	void renderLevelSelect() {
+		SDL_RenderTexture(renderer, background, NULL, &backgroundSize);
+		SDL_SetRenderDrawColor(renderer, 205, 195, 157, 255);
+
+		SDL_RenderFillRect(renderer, &tutButtonSize);
+		SDL_RenderTexture(renderer, tutButton, NULL, &tutButtonSize);
+
+		SDL_RenderFillRect(renderer, &lvl1ButtonSize);
+		SDL_RenderTexture(renderer, lvl1Button, NULL, &lvl1ButtonSize);
+
+		SDL_RenderFillRect(renderer, &lvl2ButtonSize);
+		SDL_RenderTexture(renderer, lvl2Button, NULL, &lvl2ButtonSize);
+
+		SDL_RenderFillRect(renderer, &lvl3ButtonSize);
+		SDL_RenderTexture(renderer, lvl3Button, NULL, &lvl3ButtonSize);
+
+		SDL_RenderFillRect(renderer, &conButtonSize);
+		SDL_RenderTexture(renderer, conButton, NULL, &conButtonSize);
+	}
+
 	void saveOnExit() {
 		if (lManager) {
 			lManager->saveOnExit();
@@ -102,8 +177,27 @@ private:
 
 	SDL_FRect backgroundSize{ 0, 0, camera.dimen.w, camera.dimen.h };
 	SDL_Texture* background;
+
+	// Home screen
 	SDL_FRect startButtonSize{ camera.dimen.w/2 - camera.dimen.w/10, camera.dimen.h - camera.dimen.h/10, camera.dimen.w/5, camera.dimen.h/10 };
 	SDL_Texture* startButton;
+
+	// Level select screen
+	SDL_FRect tutButtonSize{ camera.dimen.w / 3 - camera.dimen.w / 10, camera.dimen.h/3 - camera.dimen.h / 10, camera.dimen.w / 5, camera.dimen.h / 10 }; // tutorial
+	SDL_Texture* tutButton;
+
+	SDL_FRect lvl1ButtonSize{ camera.dimen.w / 4 - camera.dimen.w / 10, 2*camera.dimen.h/3 - camera.dimen.h / 10, camera.dimen.w / 5, camera.dimen.h / 10 }; // level1
+	SDL_Texture* lvl1Button;
+
+	SDL_FRect lvl2ButtonSize{ 2*camera.dimen.w / 4 - camera.dimen.w / 10, 2*camera.dimen.h/3 - camera.dimen.h / 10, camera.dimen.w / 5, camera.dimen.h / 10 }; // level2
+	SDL_Texture* lvl2Button;
+
+	SDL_FRect lvl3ButtonSize{ 3*camera.dimen.w / 4 - camera.dimen.w / 10, 2*camera.dimen.h/3 - camera.dimen.h / 10, camera.dimen.w / 5, camera.dimen.h / 10 }; // level3
+	SDL_Texture* lvl3Button;
+
+	SDL_FRect conButtonSize{ 2*camera.dimen.w / 3 - camera.dimen.w / 10, camera.dimen.h/3 - camera.dimen.h / 10, camera.dimen.w / 5, camera.dimen.h / 10 }; // continue
+	SDL_Texture* conButton;
+
 
 	LevelManager* lManager;
 
